f4e9e024a3405385de9cd3fe21a4d6286078b77b
btrx <kusumadoni77@gmail.com>
2023-03-18 16:47:19 +0700

    update express

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/result', (req, res) => {
     res.sendFile(__dirname + '/result/result2.html');
 });
 
-app.listen(3000, () => {
+app.listen(3000, 'localhost', () => {
     console.log('Server started on port 3000');
 });
